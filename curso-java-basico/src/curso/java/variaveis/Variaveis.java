4d9400f00f84e12648ee599d25b60a97259f922b
CamposLeo95 <leocampos.995@gmail.com>
2023-08-25 23:00:42 -0300

    atualizando variaveis

--- curso-java-basico/src/curso/java/variaveis/Variaveis.java
+++ curso-java-basico/src/curso/java/variaveis/Variaveis.java
@@ -2,6 +2,23 @@ package curso.java.variaveis;
 
 public class Variaveis {
     public static void main(String[] args) {
-        
+        // tipos primitivos
+        // int | byte | short | long | double | float | boolean | char | String
+
+        byte idade = 123;
+        short ano = 2023;
+        int cep = 21070033;
+        long cpf = 98765432189L;
+        float pi = 3.14F; // se for float tem que terminar com F
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeronormal = numeroCurto;
+        short numeroCurto2 = (short) numeronormal; //casting
+
+        // constantes
+        // declarada em caixa alta
+        final double PI = 3.14;
+
     }
 }
